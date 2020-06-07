494a3181e0834f1826c82e42d6975aea089a6724
IraVehementi <kevinvoneche@gmail.com>
2020-06-07 02:22:20 +0200

    login viewmodel

---
+++ Billeterie_Web/ViewModel/UserLoginForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Billeterie_Web.ViewModel
+{
+    public class UserLoginForm
+    {
+        [Required]
+        [MaxLength(25)]
+        public string Login { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(20)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
