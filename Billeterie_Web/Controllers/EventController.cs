63b9c99fcacb9ada2141831ea0ca7a6c7879f90d
IraVehementi <kevinvoneche@gmail.com>
2020-06-10 16:14:00 +0200

    EVent Controller + Modele global

---
+++ Billeterie_Web/Controllers/EventController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Billeterie_Web.Infrastructure;
+using Billeterie_Web.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Vereyon.Web;
+
+namespace Billeterie_Web.Controllers
+{
+    public class EventController : BaseController
+    {
+        public EventController(IAPIConsume apiConsume, IFlashMessage flashMessage, ISessionManager session) : base(apiConsume, flashMessage, session)
+        {
+        }
+
+        public IActionResult Index(int offset = 0)
+        {
+            //get 5 events par rapport à l'offset
+            //les process et affichage dans ma vue
+            // pagination +5 offset
+
+
+             return View();
+        }
+    }
+}
---
+++ Models/Event/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Event
+{
+    public class Event
+    {
+        int EventID { get; set; }
+        DateTime DateEvent { get; set; }
+        int LocationID { get; set; }
+        string LocationName { get; set; }
+        int ArtistID { get; set; }
+        int ArtistName { get; set; }
+    }
+}
