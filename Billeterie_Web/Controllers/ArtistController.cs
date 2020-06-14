572d3d32b278d27c32b43ef4da6c0b1b80c08157
IraVehementi <kevinvoneche@gmail.com>
2020-06-14 04:42:37 +0200

    Fiche Artiste

---
+++ Billeterie_Web/Controllers/ArtistController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Billeterie_Web.Infrastructure;
+using Billeterie_Web.Utils;
+using Billeterie_Web.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Models.Artist;
+using Vereyon.Web;
+
+namespace Billeterie_Web.Controllers
+{
+    public class ArtistController : BaseController
+    {
+        public ArtistController(IAPIConsume apiConsume, IFlashMessage flashMessage, ISessionManager session) : base(apiConsume, flashMessage, session)
+        {
+        }
+
+        public IActionResult Profil(int id)
+        {
+            ArtistViewModel avm = new ArtistViewModel();
+            avm = ConsumeInstance.Get<ArtistViewModel>("Artist/", id);
+            return View(avm);
+        }
+    }
+}
---
+++ Billeterie_Web/ViewModel/ArtistViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Models.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Billeterie_Web.ViewModel
+{
+    public class ArtistViewModel
+    {
+        public ArtistViewModel()
+        {
+            ListEvent = new List<EventArtist>();
+        }
+
+        public int ArtistID { get; set; }
+        public string ArtistName { get; set; }
+        public string ArtistDesc { get; set; }
+        public string ArtistPhoto { get; set; }
+        public List<EventArtist> ListEvent { get; set; }
+    }
+}
